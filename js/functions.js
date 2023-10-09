3f7a4799d8929ac5ad27b13125c588c2fce4384c
Alexander Bogdan <bogdan.sanyaa@mail.ru>
2023-10-09 17:48:41 +0500

    Создает функции 3

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-const isCreaterThanLength = (string, length) => (string.length<=length);
+const isCreaterThanLength = (string, length) => (string.length <= length);
 // Строка короче 5 символов
 isCreaterThanLength('abc',5); //true
 // Стркоа равна 5 символам
@@ -6,16 +6,14 @@ isCreaterThanLength('abcde'); //true
 // Сттрока длиннее 5 символов
 isCreaterThanLength('abcdef',5); //false
 
-function isPalindrome(string){
-  string=string.replaceAll(' ', '').toLowerCase();
-  let backwardWord='';
-  for(let i=string.length-1; i>=0; i--)
-  {
-    backwardWord+=string[i];
+const isPalindrome = (string) => {
+  string = string.replaceAll(' ', '').toLowerCase();
+  let backwardWord = '';
+  for(let i = string.length-1; i >= 0; i--) {
+    backwardWord += string[i];
   }
-
-  return (backwardWord===string);
-}
+  return backwardWord === string;
+};
 // Строка является палиндромом
 isPalindrome('топот'); //true
 // Несмотря на разный регистр, тоже палиндром
