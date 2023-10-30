c8a4226df0ac904dfeca1c96e6aa2ec6b9ed416a
Alexander Bogdan <bogdan.sanyaa@mail.ru>
2023-10-31 00:57:36 +0500

    Функции фозвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,10 @@
+const SECONDS_IN_HOUR = 3600;
+const SECONDS_IN_MINUTE = 60;
+const IndexTime = {
+  hours: 0,
+  minutes: 1
+};
+
 const isCreaterThanLength = (string, length) => (string.length <= length);
 // Строка короче 5 символов
 isCreaterThanLength('abc',5); //true
@@ -23,3 +30,28 @@ isPalindrome('Саша'); //true
 // Палиндром
 isPalindrome('Леша на полке клопа нашел'); //true
 
+const getSeconds = (time) => {
+  if (typeof(time) === 'string'){
+    time = time.split(':');
+    return time[IndexTime.hours] * SECONDS_IN_HOUR + time[IndexTime.minutes] * SECONDS_IN_MINUTE;
+  }
+
+  return time * SECONDS_IN_MINUTE;
+};
+
+const isPossibleMeeting = (timeStartWorkingDay, timeEndWorkingDay, timeAppointment, appointmentDuration) => {
+  timeStartWorkingDay = getSeconds(timeStartWorkingDay);
+  timeEndWorkingDay = getSeconds(timeEndWorkingDay);
+  timeAppointment = getSeconds(timeAppointment);
+  appointmentDuration = getSeconds(appointmentDuration);
+  if (timeStartWorkingDay <= timeAppointment && timeAppointment <= timeEndWorkingDay &&
+    (timeAppointment + appointmentDuration) <= timeEndWorkingDay) {
+
+    return true;
+  }
+
+  return false;
+};
+
+isPossibleMeeting('08:00', '17:30', '14:00', 90); //true
+isPossibleMeeting('08:00', '14:30', '14:00', 90); //false
