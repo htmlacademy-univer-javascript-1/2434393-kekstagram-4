56a0ab85b56592d3801239909992f052be7cbfac
Alexander Bogdan <bogdan.sanyaa@mail.ru>
2023-11-27 01:22:21 +0500

    Открывается и закрывается (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,11 +1,14 @@
 import { isEscapeKey } from './utils.js';
 
+const PORTION_COMMENTS_SHOWN = 5;
+
 const bigPictureElement = document.querySelector('.big-picture');
 const commentsList = document.querySelector('.social__comments');
 const commentListElement = commentsList.querySelector('.social__comment');
 const bodyElement = document.querySelector('body');
 const cancelButtonElement = document.querySelector('.big-picture__cancel');
-const commentCountElement = document.querySelector('.social__comment-count');
+const totalCommentsElement = document.querySelector('.comments-count');
+const commentsShownElement = document.querySelector('.comments-shown');
 const commentsLoaderElement = document.querySelector('.comments-loader');
 
 const createComment = ( {avatar, name, message} ) => {
@@ -18,15 +21,30 @@ const createComment = ( {avatar, name, message} ) => {
   return comment;
 };
 
-const renderComments = (comments) => {
+let commentsShown = 0;
+let comments = null;
+
+const renderComments = () => {
   commentsList.innerHTML='';
 
-  const fragment = document.createDocumentFragment();
-  comments.forEach((item) => {
-    const comment = createComment(item);
-    fragment.appendChild(comment);
-  });
-  commentsList.appendChild(fragment);
+  commentsShown += PORTION_COMMENTS_SHOWN;
+  if(commentsShown >= comments.length){
+    commentsShown = comments.length;
+    commentsLoaderElement.classList.add('hidden');
+  }
+  else {
+    commentsLoaderElement.classList.remove('hidden');
+  }
+  for(let i=0; i<commentsShown; i++){
+    const comment = createComment(comments[i]);
+    commentsList.appendChild(comment);
+  }
+  commentsShownElement.textContent = commentsShown;
+  totalCommentsElement.textContent = comments.length;
+};
+
+const onCommentsLoaderElementClick = () => {
+  renderComments();
 };
 
 const hideBigPicture = () => {
@@ -34,6 +52,8 @@ const hideBigPicture = () => {
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
   cancelButtonElement.removeEventListener('click', onCancelButtonClick);
+  commentsLoaderElement.removeEventListener('click', onCommentsLoaderElementClick);
+  commentsShown = 0;
 };
 
 function onDocumentKeydown (evt) {
@@ -58,12 +78,13 @@ const showBigPicture = (data) => {
   bigPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
   commentsLoaderElement.classList.add('hidden');
-  commentCountElement.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
   cancelButtonElement.addEventListener('click', onCancelButtonClick);
+  commentsLoaderElement.addEventListener('click', onCommentsLoaderElementClick);
 
   renderPictureDetails(data);
-  renderComments(data.comments);
+  comments = data.comments.slice();
+  renderComments(comments);
 };
 
 export { showBigPicture };
